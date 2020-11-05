ada86485eb63f6009f90bbb535b03756aa9e0e8d
yaro368 <jarosmichal368@gmail.com>
2020-11-05 06:43:43 +0000

    nothing changed

---
+++ pgbackup/src/pgbackup/pgdump.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
